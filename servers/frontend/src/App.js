bc85b5e3413ff2dcad28019bd7aa353264ac6d13
knamsayin <lukejordensnz@gmail.com>
2022-08-25 18:27:01 +1200

    added 404 page

--- servers/frontend/src/App.js
+++ servers/frontend/src/App.js
@@ -1,26 +1,42 @@
 import React from "react";
+import {
+  Route,
+  Routes,
+  NavLink,
+  BrowserRouter as Router,
+  Navigate
+} from "react-router-dom";
 
-/*import Home from "./pages/Home";
+import Home from "./pages/Home";
 import SEPractice from "./pages/SE-Practice";
 import SubmitArticle from "./pages/Submit-Article"; 
-import NotFoundPage from "./pages/404";*/
+import NotFoundPage from "./pages/404";
 
 const App = () =>  {
     return (
-
+      <Router>
         <div>
           <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
             <ul className="header">
-            <li><a href = "/">Home</a></li>
-                <li><a href = "/SEPractice">Select the Practice</a></li>
-                <li><a href = "/SubmitArticle">Submit an Article</a></li>
+                <li><NavLink exact to = "/">Home</NavLink></li>
+                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
+                <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
             </ul>
-          <div className="content"> 
-          
+          <div className="content">
+            <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route  path="/SEPractice" element={<SEPractice />} />
+            <Route  path="/SubmitArticle" element={<SubmitArticle />} />
+            <Route path="/404" element={<NotFoundPage />} />
+            <Route path="*" element={<Navigate replace to="/404" />} />
+            
+
+            </Routes>
           </div>
         </div>
-
+      </Router>
     );
 }
  
 export default App;
+
